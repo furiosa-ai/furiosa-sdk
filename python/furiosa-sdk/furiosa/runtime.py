f8103e48040237eaec65ccd78fe25c0b535e6ddf
Hyunsik Choi <hyunsik@furiosa.ai>
2021-03-25 02:25:06 -0700

    Improve reimport runtime

--- python/furiosa-sdk/furiosa/runtime.py
+++ python/furiosa-sdk/furiosa/runtime.py
@@ -4,3 +4,5 @@ _furiosa_sdk_runtime = importlib.util.find_spec("furiosa_sdk_runtime")
 
 if _furiosa_sdk_runtime is not None:
     from furiosa_sdk_runtime import *
+    from furiosa_sdk_runtime import __version__
+    from furiosa_sdk_runtime import __full_version__
