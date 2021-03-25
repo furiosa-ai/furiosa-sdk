3b5056e0305f341f0a4adff57ea1b1b9b7b67d18
Hyunsik Choi <hyunsik@furiosa.ai>
2021-03-25 03:22:53 -0700

    Updated the version info

--- python/furiosa-sdk-runtime/furiosa_sdk_runtime/__init__.py
+++ python/furiosa-sdk-runtime/furiosa_sdk_runtime/__init__.py
@@ -12,7 +12,7 @@ LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.INFO)
 
 
-def get_sdk_git_version():
+def _get_sdk_git_version():
     """Returns the git commit hash representing the current version of the application."""
     git_version = None
     try:
@@ -22,14 +22,14 @@ def get_sdk_git_version():
 
     return git_version
 
-
-__version__ = get_sdk_git_version()
-
-
-def full_version() -> str:
+def _full_version() -> str:
     """Returns a full version string including the native library version"""
     return "Furiosa SDK Runtime {} (libnux {} {} {})" \
         .format(__version__,
                 LIBNUX.version().decode('utf-8'),
                 LIBNUX.git_short_hash().decode('utf-8'),
                 LIBNUX.build_timestamp().decode('utf-8'))
+
+
+__version__ = _get_sdk_git_version()
+__full_version__ = _full_version()
--- python/furiosa-sdk-runtime/setup.py
+++ python/furiosa-sdk-runtime/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 logger = logging.getLogger(__name__)
 
-version = '0.1.0.dev5'
+version = '0.1.0.dev6'
 
 my_dir = dirname(__file__)
 
