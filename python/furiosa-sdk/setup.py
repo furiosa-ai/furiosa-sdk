1b6fb332fdb760f884271e2cd4a209ff6e4f1ab3
Hyunsik Choi <hyunsik@furiosa.ai>
2021-04-20 16:33:44 -0700

    Bump up the verion to 0.3.0.dev0 for development

--- python/furiosa-sdk/setup.py
+++ python/furiosa-sdk/setup.py
@@ -23,7 +23,7 @@ PREINSTALLED_PROVIDERS = []
 
 logger = logging.getLogger(__name__)
 
-version = '0.2.1'
+version = '0.3.0.dev0'
 
 my_dir = dirname(__file__)
 
@@ -47,10 +47,10 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 
 EXTRAS_REQUIREMENTS: Dict[str, List[str]] = {
-    "cli": ["furiosa-sdk-cli>=0.2.0"],
-    "runtime": ["furiosa-sdk-runtime>=0.2.0"],
-    "quantizer": ["furiosa-sdk-quantizer>=0.2.1"],
-    "validator": ["furiosa-sdk-model-validator>=0.2.1"]
+    "cli": ["furiosa-sdk-cli~=0.3.0"],
+    "runtime": ["furiosa-sdk-runtime~=0.3.0"],
+    "quantizer": ["furiosa-sdk-quantizer~=0.3.0"],
+    "validator": ["furiosa-sdk-model-validator~=0.3.0"]
 }
 
 # Requirements for all "user" extras (no devel). They are de-duplicated. Note that we do not need
