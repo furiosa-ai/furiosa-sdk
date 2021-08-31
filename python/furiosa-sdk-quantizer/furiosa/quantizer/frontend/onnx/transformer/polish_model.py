524e9b6436dfa5eb499d7b8e07eae381213e4ef5
Deokhwan Kim <dkim@furiosa.ai>
2021-08-31 17:50:44 +0900

    refactor: remove ConvertClipAttrToInput
    onnx.version_converter already plays the role of ConvertClipAttrToInput.

--- python/furiosa-sdk-quantizer/furiosa/quantizer/frontend/onnx/transformer/polish_model.py
+++ python/furiosa-sdk-quantizer/furiosa/quantizer/frontend/onnx/transformer/polish_model.py
@@ -1,7 +1,6 @@
 import onnx
 import onnxoptimizer
 
-from furiosa.quantizer.frontend.onnx.transformer.convert_clip_attr_to_input import ConvertClipAttrToInput
 from furiosa.quantizer.frontend.onnx.transformer.convert_2d_sum_to_add import Convert2dSumToAdd
 from furiosa.quantizer.frontend.onnx.utils.inference_shape import InferenceShape
 from furiosa.quantizer.frontend.onnx.transformer import utils
@@ -19,7 +18,6 @@ class PolishModel(Transformer[onnx.ModelProto]):
         model = utils.fix_batch_size_as_one(model)
 
         model = onnxoptimizer.optimize(model, passes=["extract_constant_to_initializer"])
-        model = ConvertClipAttrToInput().transform(model)
         model = Convert2dSumToAdd().transform(model)
         model = InferenceShape(model).inference_shape()
 
