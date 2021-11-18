c58bb30ab91db38b0fff4363acddd76a0a9df1ca
jason <jason@furiosa.ai>
2021-11-18 14:39:24 +0900

    raises custom RuntimeError for specified message

--- python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/utils/inference_shape.py
+++ python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/utils/inference_shape.py
@@ -2,7 +2,6 @@ from typing import Dict, List, Optional
 
 import onnx
 import onnxsim
-from onnxsim.onnx_simplifier import get_input_names
 
 from furiosa.quantizer.frontend.onnx.transformer import utils
 from furiosa.quantizer.frontend.onnx.utils.check_model import check_model
@@ -26,9 +25,18 @@ class InferenceShape:
                 skipped_optimizers=['eliminate_duplicate_initializer', 'fuse_add_bias_into_conv'],
                 input_shapes=input_shapes,
             )
-        except RuntimeError:
-            input_names = get_input_names(self.model)
-            raise RuntimeError(f"Shape(s) must be specified for graph input(s): {input_names}")
+
+        except RuntimeError as e:
+            import re
+
+            # https://github.com/daquexian/onnx-simplifier/blob/99f544e805d952ff35e682ec276c8d3cd1a377e5/onnxsim/onnx_simplifier.py#L98-L102
+            m = re.match(r'The shape of input "(\w+)" has dynamic size', e.args[0])
+            if m is not None:
+                raise RuntimeError(
+                    f'the static shape of input "{m.group(1)}" must be given'
+                ) from None
+            else:
+                raise
 
         assert check
         check_model(self.model)
