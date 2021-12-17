c1546b4b4213a7d05e6b5cb85a792f986dc8e556
jason <jason@furiosa.ai>
2021-12-17 19:58:04 +0900

    apply onnx model maker to test_convert_conv1d_to_conv2d

--- python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/transformer/convert_conv1d_to_conv2d.py
+++ python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/transformer/convert_conv1d_to_conv2d.py
@@ -18,26 +18,24 @@ class ConvertConv1dToConv2d(Transformer):
 class Pattern_1(ONNXTransformer):
     """
     transform
-        prev --> Reshape --> Conv --> Reshape --> next
+        prev --> Reshape --> Conv(1d) --> Reshape --> next
     to
-        prev --> Reshape --> Conv --> Reshape --> next
+        prev --> Reshape --> Conv(2d) --> Reshape --> next
 
-    if Conv.input[0].ndim == 3, i.e., if Conv1d
+    if Conv(1d).input[0].ndim == 3
     """
 
     pattern_to_match = ['Reshape', 'Conv', 'Reshape']
 
     def pattern_matching(self, base_node):
-        inputs = base_node.input
-
         matched_nodes = self.pattern_matcher(base_node, self.pattern_to_match)
         if not matched_nodes:
-            return inputs
+            return base_node.input
 
         if not self.pattern_condition_checker(matched_nodes):
-            return inputs
+            return base_node.input
 
-        top_node, mid_node, base_node = matched_nodes
+        reshape_0, conv1d, reshape_1 = matched_nodes
 
         # Inserts an axis of length one at the position of the H axis.
         # In other words, a tensor of shape (N, C, D) will be reshaped
@@ -47,70 +45,68 @@ class Pattern_1(ONNXTransformer):
         # tensors with a large W axis better than those with a large H
         # axis. Refer to
         # <https://github.com/furiosa-ai/dss/pull/183#issuecomment-861137438>.
-        new_mid_input_shape = self.get_value_info_shape(mid_node.input[0])
-        new_mid_input_shape.insert(-1, 1)
-        new_top_reshape_shape = self.get_initializer_array(top_node.input[1]).tolist()
-        new_top_reshape_shape.insert(-1, 1)
-        new_mid_output_shape = self.get_value_info_shape(mid_node.output[0])
-        new_mid_output_shape.insert(-1, 1)
-        new_mid_weight_shape = self.get_value_info_shape(mid_node.input[1])
-        new_mid_weight_shape.insert(-1, 1)
+        conv2d_input_shape = self.get_value_info_shape(conv1d.input[0])
+        conv2d_input_shape.insert(-1, 1)
+        conv2d_output_shape = self.get_value_info_shape(conv1d.output[0])
+        conv2d_output_shape.insert(-1, 1)
+        conv2d_weight_shape = self.get_value_info_shape(conv1d.input[1])
+        conv2d_weight_shape.insert(-1, 1)
+        new_reshape_0 = self.get_initializer_array(reshape_0.input[1]).tolist()
+        new_reshape_0.insert(-1, 1)
         self.transform_to_convert(
             matched_nodes,
             nodes_to_add=[
                 self.make_node(
                     'Reshape',
-                    [top_node.input[0], top_node.input[1] + '_converted'],
-                    [top_node.output[0]],
-                    top_node.name,
+                    [reshape_0.input[0], reshape_0.input[1] + '_converted'],
+                    [reshape_0.output[0]],
+                    reshape_0.name,
                 ),
                 self.make_node(
                     'Conv',
                     [
-                        mid_node.input[0],
-                        mid_node.input[1] + '_converted',
-                        mid_node.input[2] if len(mid_node.input) == 3 else None,
+                        conv1d.input[0],
+                        conv1d.input[1] + '_converted',
+                        conv1d.input[2] if len(conv1d.input) == 3 else None,
                     ],
-                    [mid_node.output[0]],
-                    mid_node.name,
-                    **self.get_attrs(mid_node),
+                    [conv1d.output[0]],
+                    conv1d.name,
+                    **self.get_conv2d_attrs(conv1d),
                 ),
-                base_node,
+                reshape_1,
             ],
             inits_to_add=[
                 self.make_initializer_from_array(
-                    np.array(new_top_reshape_shape), name=top_node.input[1] + '_converted'
+                    np.array(new_reshape_0), name=reshape_0.input[1] + '_converted'
                 ),
                 self.make_initializer_from_array(
-                    self.get_initializer_array(mid_node.input[1]).reshape(new_mid_weight_shape),
-                    name=mid_node.input[1] + '_converted',
+                    self.get_initializer_array(conv1d.input[1]).reshape(conv2d_weight_shape),
+                    name=conv1d.input[1] + '_converted',
                 ),
-                self.initializer_map[mid_node.input[0]] if len(mid_node.input) == 3 else None,
+                self.initializer_map[conv1d.input[0]] if len(conv1d.input) == 3 else None,
             ],
             vis_to_add=[
                 self.make_tensor_value_info(
-                    mid_node.input[0], onnx.TensorProto.FLOAT, new_mid_input_shape
+                    conv1d.input[0], onnx.TensorProto.FLOAT, conv2d_input_shape
                 ),
                 self.make_tensor_value_info(
-                    mid_node.output[0], onnx.TensorProto.FLOAT, new_mid_output_shape
+                    conv1d.output[0], onnx.TensorProto.FLOAT, conv2d_output_shape
                 ),
             ],
         )
 
-        return top_node.input
+        return reshape_0.input
 
     def pattern_condition_checker(self, nodes_to_check):
-        _, mid_node, _ = nodes_to_check
-        if len(self.get_value_info_shape(mid_node.input[0])) == 3:
-            return True
-        return False
+        _, conv1d, _ = nodes_to_check
+        return len(self.get_value_info_shape(conv1d.input[0])) == 3
 
-    def get_attrs(self, mid_node):
-        attrs = {attr.name: onnx.helper.get_attribute_value(attr) for attr in mid_node.attribute}
+    def get_conv2d_attrs(self, conv1d):
+        attrs = {attr.name: onnx.helper.get_attribute_value(attr) for attr in conv1d.attribute}
         auto_pad = attrs.get('auto_pad', 'NOTSET')
         dilations = attrs.get('dilations', [1])
         group = attrs.get('group', 1)
-        kernel_shape = attrs['kernel_shape']
+        kernel_shape = attrs.get('kernel_shape', [self.get_value_info_shape(conv1d.input[1])[-1]])
         strides = attrs.get('strides', [1])
 
         new_attrs = {
--- python/furiosa-quantizer/tests/frontend/onnx/transformer/test_convert_conv1d_to_conv2d.py
+++ python/furiosa-quantizer/tests/frontend/onnx/transformer/test_convert_conv1d_to_conv2d.py
@@ -1,44 +1,29 @@
-import torch.nn as nn
+import numpy as np
 
-from furiosa.quantizer.frontend.onnx.transformer.convert_conv1d_to_conv2d import (
-    ConvertConv1dToConv2d,
-)
+from furiosa.quantizer.frontend.onnx.transformer.convert_conv1d_to_conv2d import Pattern_1
 from tests.frontend.onnx.transformer import TestTransformer
 
 
-class UnitTestModel(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(UnitTestModel, self).__init__()
-        self.in_ch = in_channels
-        self.out_ch = out_channels
-        self.conv = nn.Conv1d(
-            in_channels=in_channels, out_channels=out_channels, kernel_size=3, bias=False
-        )
-
-    def forward(self, x):
-        x = x.reshape((1, 1, -1))
-        x = self.conv(x)
-        x = x.reshape((1, -1, 1, 1))
-        return x
-
-
 class TestConvertConv1dToConv2d(TestTransformer):
-    def _make_test_model(self, torch_model, input_shapes):
-        orig_model, trans_model = self.make_test_model(
-            torch_model, ConvertConv1dToConv2d(), input_shapes
-        )
-        return orig_model, trans_model
-
     def test_case1(self):
-        input_shapes = [(1, 16)]
-        in_channel = 1
-        out_channel = 1
+        input_shape = [1, 16]
+        output_shape = [1, 14, 1, 1]
+        model_desc = {
+            "input": {"x": (np.float32, input_shape)},
+            "output": {"y": (np.float32, output_shape)},
+            "initializer": {
+                "shape": np.array([1, 1, -1], dtype=np.int64),
+                "w": (np.float32, [1, 1, 3]),
+                "shape1": np.array([1, -1, 1, 1], dtype=np.int64),
+            },
+            "node": [
+                ("Reshape", ["x", "shape"], ["1"]),
+                ("Conv", ["1", "w"], ["2"]),
+                ("Reshape", ["2", "shape1"], ["y"]),
+            ],
+        }
 
-        op_types = ['Reshape', 'Conv', 'Reshape']
-
-        orig_model, trans_model = self._make_test_model(
-            UnitTestModel(in_channel, out_channel), input_shapes
-        )
-        self.check_graph_node(trans_model, op_types)
-        self.check_output_value(orig_model, trans_model, input_shapes)
+        orig_model, trans_model = self.make_test_model(model_desc, Pattern_1)
+        self.check_graph_node(trans_model, op_types=['Reshape', 'Conv', 'Reshape'])
+        self.check_output_value(orig_model, trans_model, [input_shape])
         self.check_value_info(trans_model)
