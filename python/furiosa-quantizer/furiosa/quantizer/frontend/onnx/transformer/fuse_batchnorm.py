92c783eda1bbbf68199eb03ee2f6b79764a24682
jason-furiosa <jason@furiosa.ai>
2022-02-22 16:51:31 +0900

    factor out common utils fuctions

--- python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/transformer/fuse_batchnorm.py
+++ python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/transformer/fuse_batchnorm.py
@@ -1,10 +1,13 @@
-from typing import List
+import logging
+from typing import List, Tuple
 
 import numpy as np
 import onnx
 
-from furiosa.quantizer.frontend.onnx.transformer import BatchNormFusion
+from furiosa.quantizer.frontend.onnx.transformer import ONNXTransformer
 from furiosa.quantizer.interfaces.transformer import Transformer
+
+logger = logging.getLogger(__name__)
 
 
 class FuseBatchNorm(Transformer):
@@ -15,7 +18,55 @@ class FuseBatchNorm(Transformer):
         return model
 
 
-class Pattern_1(BatchNormFusion):
+def _get_bn_params(
+    node: onnx.NodeProto, get_init_arr_func: classmethod
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, float]:
+    scale = get_init_arr_func(node.input[1])
+    if all(v == 0.0 for v in scale):
+        logger.warning(f'BatchNormalization.scale is a zero tensor: {node.input[1]}')
+
+    B = get_init_arr_func(node.input[2])
+    mean = get_init_arr_func(node.input[3])
+    var = get_init_arr_func(node.input[4])
+
+    eps = next(
+        (
+            onnx.helper.get_attribute_value(attr)
+            for attr in node.attribute
+            if attr.name == "epsilon"
+        ),
+        1e-05,
+    )
+
+    return scale, B, mean, var, eps
+
+
+def _fuse_bn_params(
+    weight: np.ndarray, multiplier: np.ndarray, shifter: np.ndarray, axis: int = 0
+) -> np.ndarray:
+    if weight.ndim == 4:
+        idx = [1, 1, 1]
+        idx.insert(axis, -1)
+        fused_weight = weight * multiplier.reshape(idx)
+        return fused_weight
+    elif weight.ndim == 1:
+        fused_bias = weight * multiplier + shifter
+        return fused_bias
+    else:
+        raise Exception('Unknown weight ndim: %s' % weight.dim)
+
+
+def _get_multiplier_and_shifter(
+    scale: np.ndarray, B: np.ndarray, mean: np.ndarray, var: np.ndarray, eps: float
+) -> Tuple[np.ndarray, np.ndarray]:
+    reciprocal = np.sqrt(var + eps)
+    multiplier = scale / reciprocal
+    shifter = -mean * scale / reciprocal + B
+
+    return multiplier, shifter
+
+
+class Pattern_1(ONNXTransformer):
     """
     transform
         prev --> Conv --> BatchNormalization --> next
@@ -24,6 +75,22 @@ class Pattern_1(BatchNormFusion):
     """
 
     pattern_to_match = ['Conv', 'BatchNormalization']
+
+    def pattern_matching(self, base_node: onnx.NodeProto) -> List[str]:
+        matched_nodes = self.pattern_matcher(base_node, self.pattern_to_match)
+        if not matched_nodes:
+            return base_node.input
+
+        top_node = matched_nodes[0]
+
+        self.transform_to_fuse(
+            matched_nodes,
+            nodes_to_add=self.make_new_node(matched_nodes),
+            inits_to_add=self.make_new_init(matched_nodes),
+            vis_to_add=[],
+        )
+
+        return top_node.input
 
     def make_new_node(self, matched_nodes: List[onnx.NodeProto]) -> List[onnx.NodeProto]:
         top_node, base_node = matched_nodes
@@ -35,8 +102,25 @@ class Pattern_1(BatchNormFusion):
 
         return [self.make_node('Conv', input_names, [base_node.output[0]], top_node.name)]
 
+    def make_new_init(self, matched_nodes: List[onnx.NodeProto]) -> List[onnx.TensorProto]:
+        top_node, base_node = matched_nodes
+        bn_params = _get_bn_params(base_node, self.get_initializer_array)
+        multiplier, shifter = _get_multiplier_and_shifter(*bn_params)
 
-class Pattern_2(BatchNormFusion):
+        inits_to_add = []
+        for node_input in top_node.input:
+            if node_input not in self.initializer_map:
+                continue
+            weight = self.get_initializer_array(node_input)
+            fused_weight = _fuse_bn_params(weight, multiplier, shifter)
+            inits_to_add.append(
+                self.make_initializer_from_array(fused_weight, node_input + '_bn_fused')
+            )
+
+        return inits_to_add
+
+
+class Pattern_2(ONNXTransformer):
     """
     transform
         prev --> ConvTranspose --> BatchNormalization --> next
@@ -45,6 +129,22 @@ class Pattern_2(BatchNormFusion):
     """
 
     pattern_to_match = ['ConvTranspose', 'BatchNormalization']
+
+    def pattern_matching(self, base_node: onnx.NodeProto) -> List[str]:
+        matched_nodes = self.pattern_matcher(base_node, self.pattern_to_match)
+        if not matched_nodes:
+            return base_node.input
+
+        top_node = matched_nodes[0]
+
+        self.transform_to_fuse(
+            matched_nodes,
+            nodes_to_add=self.make_new_node(matched_nodes),
+            inits_to_add=self.make_new_init(matched_nodes),
+            vis_to_add=[],
+        )
+
+        return top_node.input
 
     def make_new_node(self, matched_nodes):
         top_node, base_node = matched_nodes
@@ -64,19 +164,25 @@ class Pattern_2(BatchNormFusion):
             )
         ]
 
-    @staticmethod
-    def fuse_bn_params(weight, multiplier, shifter):
-        if weight.ndim == 4:
-            fused_weight = weight * multiplier.reshape(1, -1, 1, 1)
-            return fused_weight
-        elif weight.ndim == 1:
-            fused_bias = weight * multiplier + shifter
-            return fused_bias
-        else:
-            raise Exception('Unknown weight ndim: %s' % weight.dim)
+    def make_new_init(self, matched_nodes: List[onnx.NodeProto]) -> List[onnx.TensorProto]:
+        top_node, base_node = matched_nodes
+        bn_params = _get_bn_params(base_node, self.get_initializer_array)
+        multiplier, shifter = _get_multiplier_and_shifter(*bn_params)
+
+        inits_to_add = []
+        for node_input in top_node.input:
+            if node_input not in self.initializer_map:
+                continue
+            weight = self.get_initializer_array(node_input)
+            fused_weight = _fuse_bn_params(weight, multiplier, shifter, axis=1)
+            inits_to_add.append(
+                self.make_initializer_from_array(fused_weight, node_input + '_bn_fused')
+            )
+
+        return inits_to_add
 
 
-class Pattern_3(BatchNormFusion):
+class Pattern_3(ONNXTransformer):
     """
     transform
         prev --> Conv --> Mul --> Add --> next
@@ -103,7 +209,7 @@ class Pattern_3(BatchNormFusion):
             matched_nodes,
             nodes_to_add=self.make_new_node(matched_nodes),
             inits_to_add=self.make_new_init(matched_nodes),
-            vis_to_add=self.make_new_vi(matched_nodes),
+            vis_to_add=[],
         )
 
         return top_node.input
@@ -142,7 +248,7 @@ class Pattern_3(BatchNormFusion):
         multiplier, shifter = self.get_multiplier_and_shifter(middle_node, bottom_node)
 
         weight = self.get_initializer_array(top_node.input[1])
-        fused_weight = self.fuse_bn_params(weight, multiplier, shifter)
+        fused_weight = _fuse_bn_params(weight, multiplier, shifter)
         fused_weight_name = top_node.input[1] + '_bn_fused'
 
         if len(top_node.input) == 3:
@@ -151,7 +257,7 @@ class Pattern_3(BatchNormFusion):
         else:
             bias = np.zeros(weight.shape[0]).astype(np.float32)
             fused_bias_name = top_node.output[0] + '_bias_bn_fused'
-        fused_bias = self.fuse_bn_params(bias, multiplier, shifter)
+        fused_bias = _fuse_bn_params(bias, multiplier, shifter)
 
         return [
             self.make_initializer_from_array(fused_weight, name=fused_weight_name),
@@ -164,7 +270,7 @@ class Pattern_3(BatchNormFusion):
         return multiplier.flatten(), shifter.flatten()
 
 
-class Pattern_4(BatchNormFusion):
+class Pattern_4(ONNXTransformer):
     """
     transform
         prev --> BatchNormalization --> next
@@ -175,6 +281,22 @@ class Pattern_4(BatchNormFusion):
     """
 
     pattern_to_match = ['BatchNormalization']
+
+    def pattern_matching(self, base_node: onnx.NodeProto) -> List[str]:
+        matched_nodes = self.pattern_matcher(base_node, self.pattern_to_match)
+        if not matched_nodes:
+            return base_node.input
+
+        top_node = matched_nodes[0]
+
+        self.transform_to_fuse(
+            matched_nodes,
+            nodes_to_add=self.make_new_node(matched_nodes),
+            inits_to_add=self.make_new_init(matched_nodes),
+            vis_to_add=self.make_new_vi(matched_nodes),
+        )
+
+        return top_node.input
 
     def pattern_condition_checker(self, nodes_to_check: List[onnx.NodeProto]) -> bool:
         node = nodes_to_check[0]
@@ -202,8 +324,8 @@ class Pattern_4(BatchNormFusion):
 
     def make_new_init(self, matched_nodes: List[onnx.NodeProto]) -> List[onnx.TensorProto]:
         node = matched_nodes[0]
-        bn_params = self.get_bn_params(node)
-        multiplier, shifter = self.get_multiplier_and_shifter(*bn_params)
+        bn_params = _get_bn_params(node, self.get_initializer_array)
+        multiplier, shifter = _get_multiplier_and_shifter(*bn_params)
         num_features = self.get_value_info_shape(node.output[0])[0]
         return [
             self.make_initializer_from_array(
