38e180f7e5e9712e6eab882d0ba4164c7e3c95a7
Hyunsik Choi <hyunsik@furiosa.ai>
2022-03-23 12:36:49 -0700

    Use furiosa-tools compile function rather than session.create

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,11 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
-## [0.6.1 - unreleased]
+## [0.6.2 - unreleased]
+### Changed
+- litmus should use furiosa-tools compile rather than session.create() #317
+
+## [0.6.1]
 ### Fixed
 - Compiler report shouldn't be displayed unless running as unit tests (#306)
 - Throw an exception if session is already closed (#305)
--- python/furiosa-litmus/furiosa/litmus/__init__.py
+++ python/furiosa-litmus/furiosa/litmus/__init__.py
@@ -9,7 +9,7 @@ import onnx
 from furiosa.common.utils import eprint, get_sdk_version
 from furiosa.quantizer.frontend.onnx import post_training_quantization_with_random_calibration
 from furiosa.quantizer.frontend.onnx.quantizer.utils import QuantizationMode
-from furiosa.runtime import session
+from furiosa.tools.compiler.api import compile
 
 __version__ = get_sdk_version("furiosa.litmus")
 
@@ -21,41 +21,41 @@ def validate(model_path: Path):
     :param model_path: Model path
     :return: None
     """
-    tmpfile = tempfile.NamedTemporaryFile()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        if not model_path.exists():
+            eprint(f"ERROR: {model_path} does not exist")
 
-    if not model_path.exists():
-        eprint(f"ERROR: {model_path} does not exist")
+        # Try quantization on input models
+        print("[Step 1] Checking if the model can be transformed into a quantized model ...")
+        try:
+            quantized_model = post_training_quantization_with_random_calibration(
+                model=onnx.load_model(model_path),
+                per_channel=True,
+                static=True,
+                mode=QuantizationMode.DFG,
+                num_data=10,
+            )
+        except Exception as e:
+            eprint("[Step 1] Failed\n")
+            raise e
+        print("[Step 1] Passed")
 
-    # Try quantization on input models
-    print("[Step 1] Checking if the model can be transformed into a quantized model ...")
-    try:
-        quantized_model = post_training_quantization_with_random_calibration(
-            model=onnx.load_model(model_path),
-            per_channel=True,
-            static=True,
-            mode=QuantizationMode.DFG,
-            num_data=10,
-        )
-    except Exception as e:
-        eprint("[Step 1] Failed\n")
-        raise e
-    print("[Step 1] Passed")
+        step1_output = f"{tmpdir}/step1.onnx"
+        step2_output = f"{tmpdir}/step2.enf"
 
-    try:
-        onnx.save_model(quantized_model, tmpfile.name)
-    except Exception as e:
-        eprint("[ERROR] Fail to save the model\n")
-        raise e
+        try:
+            onnx.save_model(quantized_model, step1_output)
+        except Exception as e:
+            eprint("[ERROR] Fail to save the model\n")
+            raise e
 
-    print("[Step 2] Checking the model can be compiled to a NPU program ...")
-    try:
-        with open(tmpfile.name, "rb") as model_file:
-            model = model_file.read()
-            session.create(model=model)
-    except Exception as e:
-        eprint("[Step 2] Failed\n")
-        raise e
-    print("[Step 2] Passed")
+        print("[Step 2] Checking the model can be compiled to a NPU program ...")
+        try:
+            compile(step1_output, step2_output, verbose=True, target_npu="warboy-2pe")
+        except Exception as e:
+            eprint("[Step 2] Failed\n")
+            raise e
+        print("[Step 2] Passed")
 
 
 def main():
