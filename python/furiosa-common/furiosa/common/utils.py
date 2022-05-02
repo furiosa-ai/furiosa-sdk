e554f729f1482fd04e38fba7ba96f4e14b55f488
Youseok Yang <yan@furiosa.ai>
2022-05-02 11:22:03 +0900

    Add setuptools-scm configuration option

--- python/furiosa-common/furiosa/common/utils.py
+++ python/furiosa-common/furiosa/common/utils.py
@@ -3,12 +3,16 @@ from __future__ import print_function
 import logging as log
 import pkgutil
 import sys
-import typing
+from typing import Optional
+
+from packaging.version import Version
 
 
 class FuriosaVersionInfo:
-    def __init__(self, parts: typing.Iterable[str]):
-        self.stage, self.version, self.hash = parts
+    def __init__(self, version: Version):
+        self.version = version.base_version
+        self.stage = "dev" if version.is_devrelease else "release"
+        self.hash = next(iter(version.local.split(".")))[1:]
 
     def __str__(self):
         return f"{self.version} (rev: {self.hash[0:9]})"
@@ -17,16 +21,16 @@ class FuriosaVersionInfo:
         return f"FuriosaVersionInfo(({self.stage}, {self.version}, {self.hash}))"
 
 
-def get_sdk_version(module) -> FuriosaVersionInfo:
+def get_sdk_version(module) -> Optional[FuriosaVersionInfo]:
     """Returns the git commit hash representing the current version of the application."""
-    git_version = None
+    sdk_version = None
     try:
-        verion_string = str(pkgutil.get_data(module, 'git_version'), encoding="UTF-8")
-        git_version = FuriosaVersionInfo(verion_string.split(":"))
+        version_string = str(pkgutil.get_data(module, 'git_version.txt'), encoding="UTF-8")
+        sdk_version = FuriosaVersionInfo(Version(version_string))
     except Exception as e:  # pylint: disable=broad-except
-        log.debug(e)
+        log.warn(e)
 
-    return git_version
+    return sdk_version
 
 
 def eprint(*args, **kwargs):
