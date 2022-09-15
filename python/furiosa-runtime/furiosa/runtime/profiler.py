7196cf71eed49504692afc39ea4282f87676202a
ByungChan Lee <libc@furiosa.ai>
2022-09-15 16:58:12 +0900

    refine accessing DataFrame APIs

--- python/furiosa-runtime/furiosa/runtime/profiler.py
+++ python/furiosa-runtime/furiosa/runtime/profiler.py
@@ -29,7 +29,7 @@ class RecordFormat(IntEnum):
     """Profiler format to record profile data."""
 
     ChromeTrace = 0
-    CsvTrace = 1
+    PandasDataFrame = 1
 
 
 class ChromeTraceConfig(BaseModel):
@@ -54,12 +54,11 @@ class ChromeTraceConfig(BaseModel):
     file: io.IOBase = Field(default_factory=lambda: sys.stdout)
 
 
-class CsvTraceConfig(BaseModel):
-    """CsvTrace specific config.
+class PandasDataFrameConfig(BaseModel):
+    """PandasDataFrame specific config.
 
     Attributes:
-        cpu_file: file descriptor to write CPU profile data. By default, memfd.
-        npu_file: file descriptor to write NPU profile data. By default, memfd.
+        file: file descriptor to write profile data. By default, memfd.
     """
 
     class Config:
@@ -67,14 +66,13 @@ class CsvTraceConfig(BaseModel):
         arbitrary_types_allowed = True
 
     # Provide default value as default_factory because stdout itself is not pickle-able.
-    cpu_file: int
-    npu_file: int
+    file: int
 
 
 # Format specific configs
 configs = {
     RecordFormat.ChromeTrace: ChromeTraceConfig,
-    RecordFormat.CsvTrace: CsvTraceConfig,
+    RecordFormat.PandasDataFrame: PandasDataFrameConfig,
 }
 
 
@@ -112,22 +110,11 @@ class profile:
         self.resource = resource
         self.format = format
 
-        if format == RecordFormat.CsvTrace:
-            if Resource.CPU in resource:
-                if 'cpu_file' in config.keys():
-                    config['cpu_file'] = config['cpu_file'].fileno()
-                else:
-                    config['cpu_file'] = os.memfd_create('cpu_profiling')
+        if format == RecordFormat.PandasDataFrame:
+            if 'file' in config.keys():
+                config['file'] = config['file'].fileno()
             else:
-                config['cpu_file'] = -1
-
-            if Resource.NPU in resource:
-                if 'npu_file' in config.keys():
-                    config['npu_file'] = config['npu_file'].fileno()
-                else:
-                    config['npu_file'] = os.memfd_create('npu_profiling')
-            else:
-                config['npu_file'] = -1
+                config['file'] = os.memfd_create('profiling')
 
         self.config = configs[format](**config).json()
 
@@ -146,10 +133,7 @@ class profile:
         """
 
         self.profiler = c_void_p(None)
-        self.df = {
-            "cpu": pd.DataFrame(),
-            "npu": pd.DataFrame(),
-        }
+        self.df = pd.DataFrame()
 
         err = LIBNUX.profiler_enable(
             self.resource.value, self.format.value, self.config.encode(), byref(self.profiler)
@@ -172,24 +156,24 @@ class profile:
         """
         LIBNUX.profiler_disable(self.profiler)
 
-        if self.format == RecordFormat.CsvTrace:
+        if self.format == RecordFormat.PandasDataFrame:
             config = json.loads(self.config)
-            if 'cpu_file' in config.keys() and config['cpu_file'] >= 0:
-                pos = os.lseek(config['cpu_file'], 0, os.SEEK_CUR)
-                os.lseek(config['cpu_file'], 0, os.SEEK_SET)
-                buf = os.read(config['cpu_file'], pos)
-                self.df['cpu'] = pd.read_csv(io.StringIO(buf.decode()), dtype={"op_idx": "Int64"})
-                self.df['cpu']['dur'] = self.df['cpu']["end"] - self.df['cpu']["start"]
-
-            if 'npu_file' in config.keys() and config['npu_file'] >= 0:
-                pos = os.lseek(config['npu_file'], 0, os.SEEK_CUR)
-                os.lseek(config['npu_file'], 0, os.SEEK_SET)
-                buf = os.read(config['npu_file'], pos)
-                self.df['npu'] = pd.read_csv(
-                    io.StringIO(buf.decode()),
-                    dtype={"pe_idx": "Int64", "exec_idx": "Int64", "inst_idx": "Int64"},
-                )
-                self.df['npu']['dur'] = self.df['npu']["end"] - self.df['npu']["start"]
+            pos = os.lseek(config['file'], 0, os.SEEK_CUR)
+            os.lseek(config['file'], 0, os.SEEK_SET)
+            buf = os.read(config['file'], pos)
+            self.df = pd.read_csv(
+                io.StringIO(buf.decode()),
+                dtype={
+                    "cat": "string",
+                    "dram_base": "Int64",
+                    "pe_index": "Int64",
+                    "execution_index": "Int64",
+                    "instruction_index": "Int64",
+                    "operator_index": "Int64",
+                },
+                keep_default_na=False,
+            )
+            self.df['dur'] = self.df['end'] - self.df['start']
 
     @contextmanager
     def record(
@@ -209,135 +193,115 @@ class profile:
         LIBNUX.profiler_record_end(span)
 
     def print_npu_operators(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.NPU in self.resource:
-                print(
-                    self.df['npu'][self.df['npu']["name"] != "Execution"]
-                    .groupby(["name"])
-                    .agg({"dur": "mean", "span_id": "count"})
-                    .sort_values(by=['dur'], ascending=False)
-                    .rename(columns={'dur': 'average elapsed(ns)', 'span_id': 'count'})
-                )
+        df = self.get_npu_pandas_dataframe()
+        print(
+            df[df["name"] != "Execution"]
+            .groupby(["name"])
+            .agg({"dur": "mean", "span_id": "count"})
+            .sort_values(by=['dur'], ascending=False)
+            .rename(columns={'dur': 'average elapsed(ns)', 'span_id': 'count'})
+        )
 
     def print_npu_inference(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.NPU in self.resource:
-                execution = self.df['npu'][self.df['npu']["inst_idx"].isnull()]
-                computation = (
-                    self.df['npu'][~self.df['npu']["name"].isin(["Execution", "Load", "Store"])][
-                        ["parent_span_id", "pe_idx", "exec_idx", "dur"]
-                    ]
-                    .groupby(["parent_span_id", "pe_idx", "exec_idx"], as_index=False)
-                    .sum()
-                )
-                merged = execution.merge(computation, left_on='span_id', right_on='parent_span_id')[
-                    ["trace_id", "span_id", "pe_idx_x", "exec_idx_x", "dur_x", "dur_y"]
-                ]
-                print(
-                    merged[
-                        ["trace_id", "span_id", "pe_idx_x", "exec_idx_x", "dur_x", "dur_y"]
-                    ].rename(
-                        columns={
-                            'pe_idx_x': 'pe_idx',
-                            'exec_idx_x': 'exec_idx',
-                            'dur_x': 'NPU Total',
-                            'dur_y': 'NPU Run',
-                        }
-                    )
-                )
+        df = self.get_npu_pandas_dataframe()
+        execution = df[df["inst_idx"].isnull()]
+        computation = (
+            df[~df["name"].isin(["Execution", "Load", "Store"])][
+                ["parent_span_id", "pe_idx", "exec_idx", "dur"]
+            ]
+            .groupby(["parent_span_id", "pe_idx", "exec_idx"], as_index=False)
+            .sum()
+        )
+        merged = execution.merge(computation, left_on='span_id', right_on='parent_span_id')[
+            ["trace_id", "span_id", "pe_idx_x", "exec_idx_x", "dur_x", "dur_y"]
+        ]
+        print(
+            merged[["trace_id", "span_id", "pe_idx_x", "exec_idx_x", "dur_x", "dur_y"]].rename(
+                columns={
+                    'pe_idx_x': 'pe_idx',
+                    'exec_idx_x': 'exec_idx',
+                    'dur_x': 'NPU Total',
+                    'dur_y': 'NPU Run',
+                }
+            )
+        )
 
     def print_npu_inference_time_summary(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.NPU in self.resource:
-                execution = self.df['npu'][self.df['npu']["inst_idx"].isnull()]
-                print("Total Elapsed: " + "{:,}".format(execution["dur"].sum()) + " ns")
-                print("Average Elapsed: " + "{:,}".format(execution["dur"].mean()) + " ns")
+        df = self.get_npu_pandas_dataframe()
+        execution = df[df["inst_idx"].isnull()]
+        print("Total Elapsed: " + "{:,}".format(execution["dur"].sum()) + " ns")
+        print("Average Elapsed: " + "{:,}".format(execution["dur"].mean()) + " ns")
 
     def print_external_operators(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.CPU in self.resource:
-                print(self.df['cpu'][~self.df['cpu']["op_idx"].isnull()])
+        df = self.get_cpu_pandas_dataframe()
+        print(df[~df["op_idx"].isnull()])
 
     def print_inferences(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.CPU in self.resource:
-                print(
-                    self.df['cpu'][self.df['cpu']["name"] == "Inference"][
-                        ["trace_id", "span_id", "tid", "dur"]
-                    ]
-                )
+        df = self.get_cpu_pandas_dataframe()
+        print(df[df["name"] == "Inference"][["trace_id", "span_id", "tid", "dur"]])
 
     def print_inference_time_summary(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.CPU in self.resource:
-                inferences = self.df['cpu'][self.df['cpu']["name"] == "Inference"]
-                print("Total Elapsed: " + "{:,}".format(inferences["dur"].sum()) + " ns")
-                print("Average Elapsed: " + "{:,}".format(inferences["dur"].mean()) + " ns")
+        df = self.get_cpu_pandas_dataframe()
+        inferences = df[df["name"] == "Inference"]
+        print("Total Elapsed: " + "{:,}".format(inferences["dur"].sum()) + " ns")
+        print("Average Elapsed: " + "{:,}".format(inferences["dur"].mean()) + " ns")
+
+    def get_pandas_dataframe(self):
+        return self.df
+
+    def get_pandas_dataframe_filter_with_cat(self, column, cat):
+        return self.df[self.df[column] == cat]
 
     def get_cpu_pandas_dataframe(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.CPU in self.resource:
-                return self.df['cpu']
-
-        raise Exception("No DataFrame acquired.")
+        return self.get_pandas_dataframe_filter_with_cat("cat", "Nux")
 
     def get_npu_pandas_dataframe(self):
-        if self.format == RecordFormat.CsvTrace:
-            if Resource.NPU in self.resource:
-                return self.df['npu']
-
-        raise Exception("No DataFrame acquired.")
+        return self.get_pandas_dataframe_filter_with_cat("cat", "NPU")
 
     def export_chrome_trace(self, filename):
-        if self.format == RecordFormat.CsvTrace:
-            with open(filename, 'w') as f:
-                pid = os.getpid()
-                items = []
+        if self.df.empty:
+            print("DataFrame is empty.")
+            return
 
-                cpu_begin_time = (
-                    self.df['cpu']["start"].min() if not self.df['cpu'].empty else sys.maxsize
-                )
-                npu_begin_time = (
-                    self.df['npu']["start"].min() if not self.df['npu'].empty else sys.maxsize
-                )
-                begin_time = min(cpu_begin_time, npu_begin_time)
+        with open(filename, 'w') as f:
+            pid = os.getpid()
+            items = []
 
-                for idx, row in self.df['cpu'].iterrows():
-                    obj = {}
-                    obj['name'] = row['name']
-                    obj['cat'] = "Nux"
-                    obj['ph'] = "X"
-                    obj['ts'] = (row['start'] - begin_time) / 1000.0
-                    obj['dur'] = row['dur'] / 1000.0
-                    obj['pid'] = pid
-                    obj['tid'] = row['tid']
-                    if not pd.isna(row['op_idx']):
-                        obj['args'] = {'op_idx': row['op_idx']}
-                    items.append(obj)
+            begin_time = self.df["start"].min()
 
-                for idx, row in self.df['npu'].iterrows():
-                    obj = {}
-                    obj['name'] = row['name']
-                    obj['cat'] = "NPU"
-                    obj['ph'] = "X"
-                    obj['ts'] = (row['start'] - begin_time) / 1000.0
-                    obj['dur'] = row['dur'] / 1000.0
-                    obj['pid'] = pid
-                    obj['tid'] = row['pe_idx'] + 10000000
-                    obj['args'] = {'pe_idx': row['pe_idx'], 'exec_idx': row['exec_idx']}
-                    if not pd.isna(row['inst_idx']):
-                        obj['args']['inst_idx'] = row['inst_idx']
-                    items.append(obj)
+            for idx, row in self.df.iterrows():
+                obj = {}
+                obj['name'] = row['name']
+                obj['cat'] = row['cat']
+                obj['ph'] = "X"
+                obj['ts'] = (row['start'] - begin_time) / 1000.0
+                obj['dur'] = row['dur'] / 1000.0
+                obj['pid'] = pid
+                obj['tid'] = row['thread.id']
 
-                for row in self.df['npu']['pe_idx'].drop_duplicates():
-                    value = row.item()
-                    obj = {}
-                    obj['ph'] = "M"
-                    obj['name'] = "thread_name"
-                    obj['pid'] = pid
-                    obj['tid'] = value + 10000000
-                    obj['args'] = {'name': "NPU " + str(value)}
-                    items.append(obj)
+                if row["cat"] == "Nux":
+                    if not pd.isna(row['operator_index']):
+                        obj['args'] = {'operator_index': str(row['operator_index'])}
+                elif row["cat"] == "NPU":
+                    obj['tid'] = row['pe_index'] + 10000000
+                    obj['args'] = {
+                        'pe_index': str(row['pe_index']),
+                        'execution_index': str(row['execution_index']),
+                    }
+                    if not pd.isna(row['instruction_index']):
+                        obj['args']['instruction_index'] = str(row['instruction_index'])
 
-                json.dump(items, f)
-                print("Export trace event format completed.")
+                items.append(obj)
+
+            for row in self.get_npu_pandas_dataframe()['pe_index'].drop_duplicates():
+                value = row.item()
+                obj = {}
+                obj['ph'] = "M"
+                obj['name'] = "thread_name"
+                obj['pid'] = pid
+                obj['tid'] = value + 10000000
+                obj['args'] = {'name': "NPU " + str(value)}
+                items.append(obj)
+
+            json.dump(items, f)
+            print("Export trace event format completed.")
