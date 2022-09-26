79eda3fdc518058c65713470389d119be6109039
Youseok Yang <yan@furiosa.ai>
2022-09-26 15:52:03 +0900

    Expose tranport API in furiosa-registry

--- python/furiosa-registry/furiosa/registry/__init__.py
+++ python/furiosa-registry/furiosa/registry/__init__.py
@@ -1,6 +1,6 @@
 """FuriosaAI model registry"""
 
 # flake8: noqa
-from .client import help, list, load
+from .client import help, list, load, transport
 from .errors import RegistryError, TransportNotFound
 from .model import Format, Metadata, Model, ModelTensor, Publication, Tags
