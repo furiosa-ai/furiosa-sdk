ca086927fb096897d9cbac81605e66ab0ab48e83
Furiosa-JeongminPark <jeongmin.park@furiosa.ai>
2022-09-29 10:21:15 +0900

    reflect review

--- python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/transformer/convert_prelu_to_relu.py
+++ python/furiosa-quantizer/furiosa/quantizer/frontend/onnx/transformer/convert_prelu_to_relu.py
@@ -94,27 +94,28 @@ class Pattern_1(ONNXTransformer):
     def make_new_vi(self, matched_nodes: Iterable[onnx.NodeProto]) -> List[onnx.NodeProto]:
         (prelu,) = matched_nodes
         input_shape = self.get_value_info_shape(prelu.input[0])
+        input_dtype = self.get_value_info_dtype(prelu.input[0])
 
         return [
             onnx.helper.make_tensor_value_info(
                 prelu.output[0] + '_relu_out',
-                onnx.TensorProto.FLOAT,
+                input_dtype,
                 input_shape,
             ),
             onnx.helper.make_tensor_value_info(
                 prelu.output[0] + '_mul_0_out',
-                onnx.TensorProto.FLOAT,
+                input_dtype,
                 input_shape,
             ),
             onnx.helper.make_tensor_value_info(
                 prelu.output[0] + '_mul_1_out',
-                onnx.TensorProto.FLOAT,
+                input_dtype,
                 input_shape,
             ),
         ]
 
 
-class Pattern_2(Pattern_1):
+class Pattern_2(ONNXTransformer):
     """
     transform
         PRelu(x) = slope * x if x < 0, x if x >=0
@@ -123,6 +124,25 @@ class Pattern_2(Pattern_1):
     if
         1. PRelu's input[1] is not an initializer
     """
+
+    pattern_to_match = ['PRelu']
+
+    def pattern_matching(self, base_node: onnx.NodeProto) -> Iterable[str]:
+        matched_nodes = self.pattern_matcher(base_node, self.pattern_to_match)
+        if not matched_nodes:
+            return base_node.input
+
+        if not self.pattern_condition_checker(matched_nodes):
+            return base_node.input
+
+        self.transform_to_fuse(
+            matched_nodes,
+            nodes_to_add=self.make_new_node(matched_nodes),
+            inits_to_add=self.make_new_init(matched_nodes),
+            vis_to_add=self.make_new_vi(matched_nodes),
+        )
+
+        return base_node.input
 
     def pattern_condition_checker(self, nodes_to_check: Iterable[onnx.NodeProto]) -> bool:
         (prelu,) = nodes_to_check
@@ -173,7 +193,9 @@ class Pattern_2(Pattern_1):
         (prelu,) = matched_nodes
 
         slope_shape = self.get_value_info_shape(prelu.input[1])
-        ones_like_slope = np.ones(slope_shape).astype(np.float32)
+        slope_dtype = onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[self.get_value_info_dtype(prelu.input[1])]
+
+        ones_like_slope = np.ones(slope_shape).astype(slope_dtype)
         new_init = onnx.numpy_helper.from_array(
             ones_like_slope, prelu.output[0] + '_ones_like_slope'
         )
@@ -184,26 +206,27 @@ class Pattern_2(Pattern_1):
         (prelu,) = matched_nodes
         input_shape = self.get_value_info_shape(prelu.input[0])
         slope_shape = self.get_value_info_shape(prelu.input[1])
+        input_dtype = self.get_value_info_dtype(prelu.input[0])
 
         return [
             onnx.helper.make_tensor_value_info(
                 prelu.output[0] + '_relu_out',
-                onnx.TensorProto.FLOAT,
+                input_dtype,
                 input_shape,
             ),
             onnx.helper.make_tensor_value_info(
                 prelu.output[0] + '_slope_fused',
-                onnx.TensorProto.FLOAT,
+                input_dtype,
                 slope_shape,
             ),
             onnx.helper.make_tensor_value_info(
                 prelu.output[0] + '_mul_0_out',
-                onnx.TensorProto.FLOAT,
+                input_dtype,
                 input_shape,
             ),
             onnx.helper.make_tensor_value_info(
                 prelu.output[0] + '_mul_1_out',
-                onnx.TensorProto.FLOAT,
+                input_dtype,
                 input_shape,
             ),
         ]
