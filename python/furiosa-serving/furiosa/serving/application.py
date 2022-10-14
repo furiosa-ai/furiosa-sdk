3ce2b7e33d77c1d45df5633cfbad008567cde863
Youseok Yang <yan@furiosa.ai>
2022-10-14 15:30:46 +0900

    Add genuine async API for ServeModel

--- python/furiosa-serving/furiosa/serving/application.py
+++ python/furiosa-serving/furiosa/serving/application.py
@@ -131,6 +131,7 @@ async def nux(
     name: str,
     location: str,
     *,
+    blocking: bool = True,
     app: FastAPI,
     on_create: Callable[[ServeModel], None],
     version: Optional[str] = None,
@@ -145,6 +146,7 @@ async def nux(
     model = NPUServeModel(
         app,
         name,
+        blocking=blocking,
         model=await transport.read(fallback(location)),
         version=version,
         description=description,
