f1a495605f7d088bed7cde5843d139d80c18913e
Furiosa-JeongminPark <jeongmin.park@furiosa.ai>
2022-12-28 13:48:03 +0900

    add optimize_model inside __init__.py

--- python/furiosa-optimizer/furiosa/optimizer/__init__.py
+++ python/furiosa-optimizer/furiosa/optimizer/__init__.py
@@ -1,6 +1,7 @@
 """FuriosaAI qunatizer"""
 
 from furiosa.common.utils import get_sdk_version
+from furiosa.optimizer.frontend.onnx import optimize_model
 
 __version__ = get_sdk_version(__name__)
 
