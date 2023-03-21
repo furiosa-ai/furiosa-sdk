9c193a5456d87dcb7f9f5e200045046d7dd814e4
Yongwoo Lee <ywlee@furiosa.ai>
2023-03-21 01:17:35 +0000

    Add test.onnx model for unit test

--- python/furiosa-litmus/tests/test_litmus.py
+++ python/furiosa-litmus/tests/test_litmus.py
@@ -4,8 +4,14 @@ import unittest
 
 
 class LitmusTests(unittest.TestCase):
-    mnist_model = os.path.dirname(__file__) + '/../../../tests/data/mnist-8.onnx'
+    data_path = os.path.dirname(__file__) + '/../../../tests/data'
+    test_model = data_path + '/test.onnx'
+    mnist_model = data_path + '/mnist-8.onnx'
 
-    def test_litmus(self):
+    def test_test_model(self):
+        result = subprocess.run(['furiosa', 'litmus', self.test_model], capture_output=True)
+        self.assertTrue(result.returncode == 0, result.stderr)
+
+    def test_mnist_model(self):
         result = subprocess.run(['furiosa', 'litmus', self.mnist_model], capture_output=True)
         self.assertTrue(result.returncode == 0, result.stderr)
