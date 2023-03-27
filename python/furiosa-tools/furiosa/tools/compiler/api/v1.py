4b1c3a9ebfbd44b2f67d7e1125ef021ce22667c1
Yongwoo Lee <ywlee@furiosa.ai>
2023-03-27 05:51:55 +0000

    remove dependency on furiosa.runtime

--- python/furiosa-tools/furiosa/tools/compiler/api/v1.py
+++ python/furiosa-tools/furiosa/tools/compiler/api/v1.py
@@ -9,7 +9,6 @@ from typing import Dict, Optional, Union
 
 from furiosa.common.error import FuriosaError, is_err
 from furiosa.common.native import LogLevel, find_native_libs
-from furiosa.runtime.errors import into_exception
 
 LOG = logging.getLogger(__name__)
 
@@ -122,7 +121,27 @@ class NativeError(IntEnum):
     TFLITE_TO_DFG = 10
     USER_COMMAND = 11
     INVALID_CONFIG = 12
-    OTHER = 13
+    OTHER = 255
+
+
+class NativeException(FuriosaError):
+    _native_err: Optional[NativeError]
+
+    def __init__(self, message: str, native_err: NativeError = None):
+        self._native_err = native_err
+        super().__init__(message)
+
+    def native_error(self) -> Optional[NativeError]:
+        return self._native_err
+
+    def __repr__(self):
+        if self._native_err is None:
+            return self._message
+
+        return f'{self._message} (native error code: {self._native_err})'
+
+    def __str__(self):
+        return self.__repr__()
 
 
 class CompilerApiError(FuriosaError):
@@ -136,6 +155,100 @@ class InvalidTargetIrException(CompilerApiError):
         super().__init__(f"invalid target ir '{format}'", NativeError.INVALID_FORMAT)
 
 
+class CompilationError(NativeException):
+    def __init__(self):
+        super().__init__("Compilation error", NativeError.COMPILATION_ERROR)
+
+
+class IOError(NativeException):
+    def __init__(self):
+        super().__init__("IO error", NativeError.IO_ERROR)
+
+
+class ConfigurationMismatch(NativeException):
+    def __init__(self):
+        super().__init__("Configuration mismatch", NativeError.CONFIGURATION_MISMATCH)
+
+
+class InvalidFormat(NativeException):
+    def __init__(self):
+        super().__init__("Invalid format", NativeError.INVALID_FORMAT)
+
+
+class InvalidModel(NativeException):
+    def __init__(self):
+        super().__init__("Invalid model", NativeError.INVALID_MODEL)
+
+
+class InvalidNPUId(NativeException):
+    def __init__(self):
+        super().__init__("Invalid NPU id", NativeError.INVALID_NPUID)
+
+
+class OutOfDram(NativeException):
+    def __init__(self):
+        super().__init__("Out of dram", NativeError.OUT_OF_DRAM)
+
+
+class OutOfSram(NativeException):
+    def __init__(self):
+        super().__init__("Out of sram", NativeError.OUT_OF_SRAM)
+
+
+class OutOfInstructionMemory(NativeException):
+    def __init__(self):
+        super().__init__("Out of instruction memory", NativeError.OUT_OF_INSTUCTION_MEMORY)
+
+
+class TfliteToDFG(NativeException):
+    def __init__(self):
+        super().__init__("TFLITE to dfg", NativeError.TFLITE_TO_DFG)
+
+
+class UserCommand(NativeException):
+    def __init__(self):
+        super().__init__("User command", NativeError.USER_COMMAND)
+
+
+class InvalidConfig(NativeException):
+    def __init__(self):
+        super().__init__("Invalid config", NativeError.INVALID_CONFIG)
+
+
+class InternalError(NativeException):
+    def __init__(self):
+        super().__init__("Unknown", NativeError.OTHER)
+
+
+_errors_to_exceptions = {
+    NativeError.COMPILATION_ERROR: CompilationError(),
+    NativeError.IO_ERROR: IOError(),
+    NativeError.CONFIGURATION_MISMATCH: ConfigurationMismatch(),
+    NativeError.INVALID_FORMAT: InvalidFormat(),
+    NativeError.INVALID_MODEL: InvalidModel(),
+    NativeError.INVALID_NPUID: InvalidNPUId(),
+    NativeError.OUT_OF_DRAM: OutOfDram(),
+    NativeError.OUT_OF_SRAM: OutOfSram(),
+    NativeError.OUT_OF_INSTUCTION_MEMORY: OutOfInstructionMemory(),
+    NativeError.TFLITE_TO_DFG: TfliteToDFG(),
+    NativeError.USER_COMMAND: UserCommand(),
+    NativeError.INVALID_CONFIG: InvalidConfig(),
+    NativeError.OTHER: InternalError(),
+}
+
+
+def into_exception(err):
+    err = err.value if isinstance(err, ctypes.c_int) else err
+
+    if err == NativeError.SUCCESS:
+        return RuntimeError(msg='NuxErr.SUCCESS cannot be NuxException')
+
+    if err in _errors_to_exceptions:
+        return _errors_to_exceptions[err]
+
+    return InternalError()
+
+
 def __set_ga_param(options, key: str, value: object):
     if key.lower() == "population_size":
         if isinstance(value, int):
@@ -147,7 +260,7 @@ def __set_ga_param(options, key: str, value: object):
         if isinstance(value, int):
             LIBCOMPILER.fc_options_ga_generation_limit(options, value)
         else:
-            CompilerApiError("generation_limit must be a positive integer")
+            raise CompilerApiError("generation_limit must be a positive integer")
 
     elif key.lower() == "max_prefetch_size":
         if isinstance(value, int):
