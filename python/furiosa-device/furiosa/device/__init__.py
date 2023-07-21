a10f047d7f17eb9acacfdd3fa96bc388daee873b
Yongwoo Lee <ywlee@furiosa.ai>
2023-07-21 13:33:39 +0900

    update

--- python/furiosa-device/furiosa/device/__init__.py
+++ python/furiosa-device/furiosa/device/__init__.py
@@ -7,20 +7,15 @@ from furiosa.common.utils import get_sdk_version
 sdk_version = get_sdk_version(__name__)
 
 
-def full_version() -> str:
-    """Returns a full version string including the native library version"""
-    return "Furiosa SDK Device {} (furiosa_native_device {} {} {})".format(
-        sdk_version,
-        __version__,
-        __git_short_hash__,
-        __build_timestamp__,
-    )
-
-
-__full_version__ = full_version()
+__full_version__ = "Furiosa SDK Device {} (furiosa_native_device {} {} {})".format(
+    sdk_version,
+    __version__,
+    __git_short_hash__,
+    __build_timestamp__,
+)
 __version__ = sdk_version
 
 
-del get_sdk_version, full_version
+del get_sdk_version
 
 __all__ = ["__version__", "__full_version__"]
