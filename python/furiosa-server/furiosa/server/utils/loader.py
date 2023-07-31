385542a8e96d1c4234850b789b3fd5c624e3e8c1
Myeong-geun Shin <mg.shin@furiosa.ai>
2023-07-31 15:15:38 +0900

    fix: use new-api instead of deprecated one

--- python/furiosa-server/furiosa/server/utils/loader.py
+++ python/furiosa-server/furiosa/server/utils/loader.py
@@ -14,10 +14,10 @@ def load_model_config(stream: Union[TextIOWrapper, Dict]) -> List[ModelConfig]:
     def config(model):
         return NPUModelConfig if model["platform"] == "npu" else ModelConfig
 
-    return [config(model).parse_obj(model) for model in source["model_config_list"]]
+    return [config(model).model_validate(model) for model in source["model_config_list"]]
 
 
 def load_server_config(stream: Union[TextIOWrapper, Dict]) -> ServerConfig:
     """Load a server config from opened file (file-like object) or Python dictionary."""
     source = stream if isinstance(stream, MutableMapping) else yaml.safe_load(stream)
-    return ServerConfig.parse_obj(source)
+    return ServerConfig.model_validate(source)
