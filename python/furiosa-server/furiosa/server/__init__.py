d6170d526e84678bcaa6c2ef261154829de5d8dd
Myeong-geun Shin <mg.shin@furiosa.ai>
2023-08-02 18:39:01 +0900

    fix: add missing comma

--- python/furiosa-server/furiosa/server/__init__.py
+++ python/furiosa-server/furiosa/server/__init__.py
@@ -1,9 +1,9 @@
 """FuriosaAI model server interacting Furiosa NPU."""
 
 from .errors import ModelNotFound, ModelServerError
-from .model import AsyncNuxModel, CPUModel, Model, NPUModel, NuxModel, OpenVINOModel  # noqa: F401
+from .model import AsyncNuxModel, CPUModel, Model, NPUModel, NuxModel, OpenVINOModel
 from .server import ModelServer
-from .settings import ModelConfig, NPUModelConfig, OpenVINOModelConfig, ServerConfig  # noqa: F401
+from .settings import ModelConfig, NPUModelConfig, OpenVINOModelConfig, ServerConfig
 
 __all__ = [
     # Server
@@ -15,7 +15,7 @@ __all__ = [
     "OpenVINOModel",
     # Deprecated Model
     "NuxModel",
-    "AsyncNuxModel"
+    "AsyncNuxModel",
     # Config
     "ModelConfig",
     "NPUModelConfig",
