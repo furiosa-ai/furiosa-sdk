23336d553c61889cbe41120c4e2f98346da4ed1a
ByungChan Lee <libc@furiosa.ai>
2023-11-24 17:42:42 +0900

    Bump up version to 0.10.1

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -27,10 +27,10 @@ sys.path.append(os.path.abspath('../../../python/furiosa-serving/'))
 project = 'Furiosa SDK Documentation'
 copyright = '2023 FuriosaAI, Inc.'
 author = 'FuriosaAI, Inc.'
-version = "0.10.1rc0"
+version = "0.10.1"
 
 # The full version, including alpha/beta/rc tags
-release = "0.10.1rc0"
+release = "0.10.1"
 
 # -- General configuration ---------------------------------------------------
 
